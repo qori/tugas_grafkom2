9e96a92ce030c30e7d29849e97ed5861aae86bd8
Vian Arwanda <arwanda_vian@yahoo.com>
2012-05-20 03:04:18 +0700

    update fungsi lingkaran untuk membuat awan

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 /*      Nur Mufmin - 10108447     */
 /*     Vian Arwanda - 10108467    */
 /**********************************/
+
 #include <cstdlib>
 #include <iostream>
 #include <Windows.h>
@@ -14,6 +15,8 @@
 
 using namespace std;
 static int elbow = 20, up = 0, look_right = 0;
+const double PI = 3.141592653589793;
+int i, radius, jumlah_titik, x_tengah, y_tengah;
 
 void init(void) {
 	GLfloat mat_specular[] = { 1.0, 1.0, 1.0, 1.0 };
@@ -38,35 +41,135 @@ void init(void) {
 	glEnable(GL_DEPTH_TEST);*/
 }
 
+void renderLingkaran() {
+	glBegin(GL_POLYGON);
+		radius = 15;
+		jumlah_titik = 20;
+		x_tengah = 50;
+		y_tengah = 50;
+		
+		for (i = 0; i <= 360; i++){
+		    float sudut = i * (2 * PI / jumlah_titik);
+			float x = x_tengah + radius * cos(sudut);
+			float y = y_tengah + radius * sin(sudut);
+			glVertex2f(x / 100,y / 100);
+		}
+	glEnd();
+}
+
 void display(void) {
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
+	glColor3f(0.3, 0.6, 0.7); /* warna awan */
+	
+	/* -- begin awan 1 -- */
 	glPushMatrix();
+	glTranslatef(-1.09, 0.0, 0.0);
+	renderLingkaran();
+	glPopMatrix();
+
+	glPushMatrix();
+	glTranslatef(-1.19, -0.14, 0.0);
+	renderLingkaran();
+	glPopMatrix();
+
+	glPushMatrix();
+	glTranslatef(-1.04, -0.19, 0.0);
+	renderLingkaran();
+	glPopMatrix();
+
+	glPushMatrix();
+	glTranslatef(-0.94, -0.12, 0.0);
+	renderLingkaran();
+	glPopMatrix();
+	/* -- end awan 1 -- */
+
+	/* -- begin awan 2 -- */
+	glPushMatrix();
+	glTranslatef(-0.02, 0.5, 0.0);
+	renderLingkaran();
+	glPopMatrix();
+
+	glPushMatrix();
+	glTranslatef(-0.12, 0.46, 0.0);
+	renderLingkaran();
+	glPopMatrix();
+
+	glPushMatrix();
+	glTranslatef(-0.21, 0.36, 0.0);
+	renderLingkaran();
+	glPopMatrix();
+
+	glPushMatrix();
+	glTranslatef(-0.09, 0.36, 0.0);
+	renderLingkaran();
+	glPopMatrix();
+
+	glPushMatrix();
+	glTranslatef(-0.02, 0.32, 0.0);
+	renderLingkaran();
+	glPopMatrix();
+
+	glPushMatrix();
+	glTranslatef(0.1, 0.34, 0.0);
+	renderLingkaran();
+	glPopMatrix();
+
+	glPushMatrix();
+	glTranslatef(0.09, 0.44, 0.0);
+	renderLingkaran();
+	glPopMatrix();
+	/* -- end awan 2 -- */
+
+	glPushMatrix();
+	glColor3f(1.0, 1.0, 1.0);
 	glRotatef((GLfloat) elbow, 0.0, 0.0, 1.0);
 	glScalef(-2.0, 0.15, 0.2);
 	glutSolidCube(1.2);
 	glPopMatrix();
 
 	glPushMatrix();
+	glColor3f(1.0, 1.0, 1.0);
 	glRotatef((GLfloat) elbow, 0.0, 0.0, 1.0);
 	glScalef(0.15, -2.0, 0.2);
 	glutSolidCube(1.2);
 	glPopMatrix();
 
 	glPushMatrix();
+	glColor3f(1.0, 1.0, 1.0);
 	glTranslatef(0.0, -1.3, 0.0);
 	glutWireCube(1.0);
 	glPopMatrix();
 
 	glPushMatrix();
+	glColor3f(1.0, 1.0, 1.0);
 	glTranslatef(0.0, -0.9, 0.0);
 	glScalef(0.1, -2.0, 0.2);
 	glutWireCube(1.0);
 	glPopMatrix();
 
 	glPushMatrix();
+	glColor3f(1.0, 1.0, 1.0);
 	glutSolidSphere(0.15, 30.0, 20.0);
 	glPopMatrix();
+
+	/* Lingkaran */
+	/*glColor3f(1.0, 1.0, 1.0);
+
+	glBegin(GL_POLYGON);
+		radius = 40;
+		jumlah_titik = 20;
+		x_tengah = 50;
+		y_tengah = 50;
+		
+		for (i = 0; i <= 360; i++){
+		    float sudut = i * (2 * PI / jumlah_titik);
+			float x = x_tengah + radius * cos(sudut);
+			float y = y_tengah + radius * sin(sudut);
+			glVertex2f(x / 100,y / 100);
+		}
+	glEnd();
+	/* Lingkaran */
 
 	glFlush();
 }
@@ -114,8 +217,8 @@ int main(int argc, char** argv) {
 	cout << "Tekan s untuk Putar kincir angin ke arah kiri" << endl;
 	cout << "Tekan e untuk Putar kincir angin ke arah kanan" << endl;
 	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB | GLUT_DEPTH);
-	glutInitWindowSize(600, 600);
-	glutInitWindowPosition(100, 100);
+	glutInitWindowSize(700, 700);
+	glutInitWindowPosition(20, 20);
 	glutCreateWindow("Kincir Angin");
 	init();
 	glutDisplayFunc(display);
